26cdb27fefb1640ad0b3120cfd910e5e2982d4e4
sonamtenzindorji <61184116+sonamtenzindorji@users.noreply.github.com>
2020-03-05 16:25:40 +0600

    Update login.java

--- login.java
+++ login.java
@@ -1,4 +1,15 @@
  class Employment{
   String name;
   Integer age;
+  
+ public  Employment(String name,Integer age){
+   this.name=name;
+   this.age=age;
+     }
+  String getName(){
+  return name;
+   
+   Integer getAge(){}
+   return age;
   }
+ }
